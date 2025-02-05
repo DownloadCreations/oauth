eaa261625b20f8340cab98682e5b70a417478cf1
David Carboni <david@carboni.io>
2025-02-05 11:06:33 +0000

    Show auth code

--- api/src/routes/auth.ts
+++ api/src/routes/auth.ts
@@ -2,8 +2,12 @@ import { Request, Response } from '@scloud/lambda-api/dist/types';
 
 export async function auth(request: Request): Promise<Response> {
   console.log('auth', request.query);
+
+  const code = request.query.code;
+
+  // Response
   return {
     statusCode: 200,
-    body: { message: 'auth' },
+    body: { code },
   };
 }
